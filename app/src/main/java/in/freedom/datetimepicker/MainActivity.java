e736bf0314cb9efbafbf7bf92c910d8c42b3a128
pramod-android <40615535+pramod-android@users.noreply.github.com>
2019-02-27 09:55:49 +0530

    Initial commit

---
+++ app/src/main/java/in/freedom/datetimepicker/MainActivity.java
@@ -0,0 +1,98 @@
+package in.freedom.datetimepicker;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    String time;
+
+    EditText edtDate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtDate=(EditText)findViewById(R.id.editTextDate);
+
+        edtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GetDateTime();
+            }
+        });
+
+    }
+
+
+    private void GetDateTime() {
+
+        final Calendar myCalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                String myFormat = "dd MMM yyyy";
+
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+
+                String date=sdf.format(myCalendar.getTime());
+                GetTime(date);
+            }
+        };
+        // TODO Auto-generated method stub
+        DatePickerDialog dp = new DatePickerDialog(MainActivity.this, date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH));
+
+        dp.show();
+
+    }
+
+
+    private void GetTime(final String dt) {
+        int mHour;
+        int mMinute;
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        time = hourOfDay + " " + minute;
+                        boolean isPM = (hourOfDay >= 12);
+
+                        edtDate.setText(dt + " " + String.format("%02d:%02d %s", (hourOfDay == 12 || hourOfDay == 0) ? 12 : hourOfDay % 12, minute, isPM ? "PM" : "AM"));
+
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+
+}
